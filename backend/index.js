6188ef570462039551057fe12722302eb39b36ea
Szarewa <sfarisizarewa@gmail.com>
2025-10-18 13:16:04 +0100

    install backend dependencies, add index.js and databse.js

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { supabase } from './database.js';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Listening....')
+})
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+})
