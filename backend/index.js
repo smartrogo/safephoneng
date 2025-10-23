e49b939e217e68020ab0f7794c236666aa99b489
Szarewa <sfarisizarewa@gmail.com>
2025-10-23 21:05:15 +0100

    add required backend folders

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { supabase } from './database.js';
+import { supabase } from './config/database.js';
 
 dotenv.config();
 
