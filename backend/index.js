8f28bfefb4e9f594192adac29f763788fcc90277
Szarewa <sfarisizarewa@gmail.com>
2025-10-25 15:41:25 +0100

    modifiy package.json, added start script for backend, create phoneRoutes and userRoutes for phone_registration and profiles respectively

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,17 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { supabase } from './config/database.js';
 
+import userRoutes from './routes/userRoutes.js';
+import phoneRoutes from './routes/phoneRoutes.js';
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use('api/user', userRoutes);
+app.use('api/phones', phoneRoutes);
 
 app.get('/', (req, res) => {
     res.send('Listening....')
